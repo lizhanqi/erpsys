ad1b0f4f2668dfb480c1e3c791bcf4597a22136b
lizhanqi <lizhanqihd@163.com>
2018-05-30 21:33:15 +0800

    style:首页的预约进店实体漏提交

---
+++ app/src/main/java/com/suxuantech/erpsys/entity/MakeAnIntoStoreEntity.java
@@ -0,0 +1,637 @@
+package com.suxuantech.erpsys.entity;
+
+/**
+ * ......................我佛慈悲....................
+ * ......................_oo0oo_.....................
+ * .....................o8888888o....................
+ * .....................88" . "88....................
+ * .....................(| -_- |)....................
+ * .....................0\  =  /0....................
+ * ...................___/`---'\___..................
+ * ..................' \\|     |// '.................
+ * ................./ \\|||  :  |||// \..............
+ * .............../ _||||| -卍-|||||- \..............
+ * ..............|   | \\\  -  /// |   |.............
+ * ..............| \_|  ''\---/''  |_/ |.............
+ * ..............\  .-\__  '-'  ___/-. /.............
+ * ............___'. .'  /--.--\  `. .'___...........
+ * .........."" '<  `.___\_<|>_/___.' >' ""..........
+ * ........| | :  `- \`.;`\ _ /`;.`/ - ` : | |.......
+ * ........\  \ `_.   \_ __\ /__ _/   .-` /  /.......
+ * ....=====`-.____`.___ \_____/___.-`___.-'=====....
+ * ......................`=---='.....................
+ * ..................佛祖开光 ,永无BUG................
+ *
+ * @author Created by 李站旗 on 2018/5/30 0030 17:17 .
+ * QQ:1032992210
+ * E-mail:lizhanqihd@163.com
+ * @Description: todo(用一句话描述该文件做什么)
+ */
+public class MakeAnIntoStoreEntity extends BaseResult2<MakeAnIntoStoreEntity.DataBean> {
+
+    public static class DataBean {
+        /**
+         * id : 248
+         * customer_number : 5dca49ef-96e5-4b5c-8782-758811e299d0
+         * customer_name : 杨柳
+         * customer_sex : 女
+         * customer_tel : 13689752431
+         * customer_wechat : wkdagpt andj and
+         * customer_weibo : null
+         * customer_qq : 1535786548
+         * province_id : null
+         * province : null
+         * city_id : null
+         * city : null
+         * county_id : null
+         * county : null
+         * customer_address : 盘锦红海滩
+         * mate_name :
+         * mate_sex :
+         * mate_tel :
+         * mate_wechat :
+         * mate_weibo : null
+         * mate_qq :
+         * consultation_type : 艺术照
+         * customer_cource : 网络,老顾客,转介绍
+         * customer_intention : 中
+         * customer_orderaddress : 盘锦店
+         * customer_area : 普通区,-四楼
+         * op_signinfo : null
+         * op_signday : null
+         * customer_birthday : 19970523
+         * mate_birthday :
+         * wedding_date : null
+         * dj_staff : 春娜
+         * fp_staff : null
+         * sales_staff : 小雪
+         * dj_day : 2018-05-23 00:00:00.000
+         * fp_day : null
+         * yjd_day : 2018-05-30 00:00:00.000
+         * syjd_day : null
+         * yp_day : 2018-07-26 00:00:00.000
+         * order_day : null
+         * order_number : null
+         * last_trackday : null
+         * creator_day : 2018-05-23 13:52:00.000
+         * customer_remark :
+         * shop_name : 沈阳时尚经典婚纱店
+         * shop_code : ZX002
+         * setkey :
+         * is_fp : 未分配
+         * is_intostore :
+         * is_success :
+         * is_valid :
+         * is_loss :
+         * statusremark : null
+         * shop_codeZD : null
+         * shop_nameZD : null
+         * from_index : 15270547337519
+         * customer_from : APP
+         * brandid : 1
+         * xingming : 杨柳
+         * shouji : 13689752431
+         */
+
+        private int id;
+        private String customer_number;
+        private String customer_name;
+        private String customer_sex;
+        private String customer_tel;
+        private String customer_wechat;
+        private Object customer_weibo;
+        private String customer_qq;
+        private Object province_id;
+        private Object province;
+        private Object city_id;
+        private Object city;
+        private Object county_id;
+        private Object county;
+        private String customer_address;
+        private String mate_name;
+        private String mate_sex;
+        private String mate_tel;
+        private String mate_wechat;
+        private Object mate_weibo;
+        private String mate_qq;
+        private String consultation_type;
+        private String customer_cource;
+        private String customer_intention;
+        private String customer_orderaddress;
+        private String customer_area;
+        private Object op_signinfo;
+        private Object op_signday;
+        private String customer_birthday;
+        private String mate_birthday;
+        private Object wedding_date;
+        private String dj_staff;
+        private Object fp_staff;
+        private String sales_staff;
+        private String dj_day;
+        private Object fp_day;
+        private String yjd_day;
+        private Object syjd_day;
+        private String yp_day;
+        private Object order_day;
+        private Object order_number;
+        private Object last_trackday;
+        private String creator_day;
+        private String customer_remark;
+        private String shop_name;
+        private String shop_code;
+        private String setkey;
+        private String is_fp;
+        private String is_intostore;
+        private String is_success;
+        private String is_valid;
+        private String is_loss;
+        private Object statusremark;
+        private Object shop_codeZD;
+        private Object shop_nameZD;
+        private String from_index;
+        private String customer_from;
+        private int brandid;
+        private String xingming;
+        private String shouji;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getCustomer_number() {
+            return customer_number;
+        }
+
+        public void setCustomer_number(String customer_number) {
+            this.customer_number = customer_number;
+        }
+
+        public String getCustomer_name() {
+            return customer_name;
+        }
+
+        public void setCustomer_name(String customer_name) {
+            this.customer_name = customer_name;
+        }
+
+        public String getCustomer_sex() {
+            return customer_sex;
+        }
+
+        public void setCustomer_sex(String customer_sex) {
+            this.customer_sex = customer_sex;
+        }
+
+        public String getCustomer_tel() {
+            return customer_tel;
+        }
+
+        public void setCustomer_tel(String customer_tel) {
+            this.customer_tel = customer_tel;
+        }
+
+        public String getCustomer_wechat() {
+            return customer_wechat;
+        }
+
+        public void setCustomer_wechat(String customer_wechat) {
+            this.customer_wechat = customer_wechat;
+        }
+
+        public Object getCustomer_weibo() {
+            return customer_weibo;
+        }
+
+        public void setCustomer_weibo(Object customer_weibo) {
+            this.customer_weibo = customer_weibo;
+        }
+
+        public String getCustomer_qq() {
+            return customer_qq;
+        }
+
+        public void setCustomer_qq(String customer_qq) {
+            this.customer_qq = customer_qq;
+        }
+
+        public Object getProvince_id() {
+            return province_id;
+        }
+
+        public void setProvince_id(Object province_id) {
+            this.province_id = province_id;
+        }
+
+        public Object getProvince() {
+            return province;
+        }
+
+        public void setProvince(Object province) {
+            this.province = province;
+        }
+
+        public Object getCity_id() {
+            return city_id;
+        }
+
+        public void setCity_id(Object city_id) {
+            this.city_id = city_id;
+        }
+
+        public Object getCity() {
+            return city;
+        }
+
+        public void setCity(Object city) {
+            this.city = city;
+        }
+
+        public Object getCounty_id() {
+            return county_id;
+        }
+
+        public void setCounty_id(Object county_id) {
+            this.county_id = county_id;
+        }
+
+        public Object getCounty() {
+            return county;
+        }
+
+        public void setCounty(Object county) {
+            this.county = county;
+        }
+
+        public String getCustomer_address() {
+            return customer_address;
+        }
+
+        public void setCustomer_address(String customer_address) {
+            this.customer_address = customer_address;
+        }
+
+        public String getMate_name() {
+            return mate_name;
+        }
+
+        public void setMate_name(String mate_name) {
+            this.mate_name = mate_name;
+        }
+
+        public String getMate_sex() {
+            return mate_sex;
+        }
+
+        public void setMate_sex(String mate_sex) {
+            this.mate_sex = mate_sex;
+        }
+
+        public String getMate_tel() {
+            return mate_tel;
+        }
+
+        public void setMate_tel(String mate_tel) {
+            this.mate_tel = mate_tel;
+        }
+
+        public String getMate_wechat() {
+            return mate_wechat;
+        }
+
+        public void setMate_wechat(String mate_wechat) {
+            this.mate_wechat = mate_wechat;
+        }
+
+        public Object getMate_weibo() {
+            return mate_weibo;
+        }
+
+        public void setMate_weibo(Object mate_weibo) {
+            this.mate_weibo = mate_weibo;
+        }
+
+        public String getMate_qq() {
+            return mate_qq;
+        }
+
+        public void setMate_qq(String mate_qq) {
+            this.mate_qq = mate_qq;
+        }
+
+        public String getConsultation_type() {
+            return consultation_type;
+        }
+
+        public void setConsultation_type(String consultation_type) {
+            this.consultation_type = consultation_type;
+        }
+
+        public String getCustomer_cource() {
+            return customer_cource;
+        }
+
+        public void setCustomer_cource(String customer_cource) {
+            this.customer_cource = customer_cource;
+        }
+
+        public String getCustomer_intention() {
+            return customer_intention;
+        }
+
+        public void setCustomer_intention(String customer_intention) {
+            this.customer_intention = customer_intention;
+        }
+
+        public String getCustomer_orderaddress() {
+            return customer_orderaddress;
+        }
+
+        public void setCustomer_orderaddress(String customer_orderaddress) {
+            this.customer_orderaddress = customer_orderaddress;
+        }
+
+        public String getCustomer_area() {
+            return customer_area;
+        }
+
+        public void setCustomer_area(String customer_area) {
+            this.customer_area = customer_area;
+        }
+
+        public Object getOp_signinfo() {
+            return op_signinfo;
+        }
+
+        public void setOp_signinfo(Object op_signinfo) {
+            this.op_signinfo = op_signinfo;
+        }
+
+        public Object getOp_signday() {
+            return op_signday;
+        }
+
+        public void setOp_signday(Object op_signday) {
+            this.op_signday = op_signday;
+        }
+
+        public String getCustomer_birthday() {
+            return customer_birthday;
+        }
+
+        public void setCustomer_birthday(String customer_birthday) {
+            this.customer_birthday = customer_birthday;
+        }
+
+        public String getMate_birthday() {
+            return mate_birthday;
+        }
+
+        public void setMate_birthday(String mate_birthday) {
+            this.mate_birthday = mate_birthday;
+        }
+
+        public Object getWedding_date() {
+            return wedding_date;
+        }
+
+        public void setWedding_date(Object wedding_date) {
+            this.wedding_date = wedding_date;
+        }
+
+        public String getDj_staff() {
+            return dj_staff;
+        }
+
+        public void setDj_staff(String dj_staff) {
+            this.dj_staff = dj_staff;
+        }
+
+        public Object getFp_staff() {
+            return fp_staff;
+        }
+
+        public void setFp_staff(Object fp_staff) {
+            this.fp_staff = fp_staff;
+        }
+
+        public String getSales_staff() {
+            return sales_staff;
+        }
+
+        public void setSales_staff(String sales_staff) {
+            this.sales_staff = sales_staff;
+        }
+
+        public String getDj_day() {
+            return dj_day;
+        }
+
+        public void setDj_day(String dj_day) {
+            this.dj_day = dj_day;
+        }
+
+        public Object getFp_day() {
+            return fp_day;
+        }
+
+        public void setFp_day(Object fp_day) {
+            this.fp_day = fp_day;
+        }
+
+        public String getYjd_day() {
+            return yjd_day;
+        }
+
+        public void setYjd_day(String yjd_day) {
+            this.yjd_day = yjd_day;
+        }
+
+        public Object getSyjd_day() {
+            return syjd_day;
+        }
+
+        public void setSyjd_day(Object syjd_day) {
+            this.syjd_day = syjd_day;
+        }
+
+        public String getYp_day() {
+            return yp_day;
+        }
+
+        public void setYp_day(String yp_day) {
+            this.yp_day = yp_day;
+        }
+
+        public Object getOrder_day() {
+            return order_day;
+        }
+
+        public void setOrder_day(Object order_day) {
+            this.order_day = order_day;
+        }
+
+        public Object getOrder_number() {
+            return order_number;
+        }
+
+        public void setOrder_number(Object order_number) {
+            this.order_number = order_number;
+        }
+
+        public Object getLast_trackday() {
+            return last_trackday;
+        }
+
+        public void setLast_trackday(Object last_trackday) {
+            this.last_trackday = last_trackday;
+        }
+
+        public String getCreator_day() {
+            return creator_day;
+        }
+
+        public void setCreator_day(String creator_day) {
+            this.creator_day = creator_day;
+        }
+
+        public String getCustomer_remark() {
+            return customer_remark;
+        }
+
+        public void setCustomer_remark(String customer_remark) {
+            this.customer_remark = customer_remark;
+        }
+
+        public String getShop_name() {
+            return shop_name;
+        }
+
+        public void setShop_name(String shop_name) {
+            this.shop_name = shop_name;
+        }
+
+        public String getShop_code() {
+            return shop_code;
+        }
+
+        public void setShop_code(String shop_code) {
+            this.shop_code = shop_code;
+        }
+
+        public String getSetkey() {
+            return setkey;
+        }
+
+        public void setSetkey(String setkey) {
+            this.setkey = setkey;
+        }
+
+        public String getIs_fp() {
+            return is_fp;
+        }
+
+        public void setIs_fp(String is_fp) {
+            this.is_fp = is_fp;
+        }
+
+        public String getIs_intostore() {
+            return is_intostore;
+        }
+
+        public void setIs_intostore(String is_intostore) {
+            this.is_intostore = is_intostore;
+        }
+
+        public String getIs_success() {
+            return is_success;
+        }
+
+        public void setIs_success(String is_success) {
+            this.is_success = is_success;
+        }
+
+        public String getIs_valid() {
+            return is_valid;
+        }
+
+        public void setIs_valid(String is_valid) {
+            this.is_valid = is_valid;
+        }
+
+        public String getIs_loss() {
+            return is_loss;
+        }
+
+        public void setIs_loss(String is_loss) {
+            this.is_loss = is_loss;
+        }
+
+        public Object getStatusremark() {
+            return statusremark;
+        }
+
+        public void setStatusremark(Object statusremark) {
+            this.statusremark = statusremark;
+        }
+
+        public Object getShop_codeZD() {
+            return shop_codeZD;
+        }
+
+        public void setShop_codeZD(Object shop_codeZD) {
+            this.shop_codeZD = shop_codeZD;
+        }
+
+        public Object getShop_nameZD() {
+            return shop_nameZD;
+        }
+
+        public void setShop_nameZD(Object shop_nameZD) {
+            this.shop_nameZD = shop_nameZD;
+        }
+
+        public String getFrom_index() {
+            return from_index;
+        }
+
+        public void setFrom_index(String from_index) {
+            this.from_index = from_index;
+        }
+
+        public String getCustomer_from() {
+            return customer_from;
+        }
+
+        public void setCustomer_from(String customer_from) {
+            this.customer_from = customer_from;
+        }
+
+        public int getBrandid() {
+            return brandid;
+        }
+
+        public void setBrandid(int brandid) {
+            this.brandid = brandid;
+        }
+
+        public String getXingming() {
+            return xingming;
+        }
+
+        public void setXingming(String xingming) {
+            this.xingming = xingming;
+        }
+
+        public String getShouji() {
+            return shouji;
+        }
+
+        public void setShouji(String shouji) {
+            this.shouji = shouji;
+        }
+    }
+}
